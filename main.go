32df7ccb59b5f00a07544cadf666d29beb452024
Serge Toro <sombr@users.noreply.github.com>
2023-07-03 19:14:48 +0100

    dummy main and comments

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// Hey there, see actual code in the following branches:
+	// * v1-simple-steps -- for the step-based simulation
+	// * v2-event-sim    -- for event-time simulation
+}
